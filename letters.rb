4350b46438ce8499fa784915c7c85e297d909c3f
S Jiang <shirleyjiang@berkeley.edu>
2016-12-19 20:02:50 -0800

    Chapters 1-2

---
+++ letters.rb
@@ -0,0 +1,12 @@
+puts 'I like' + ' apple pie'
+puts 'blink ' * 4
+puts 12+12
+puts '12'+'12'
+puts '12+12'
+puts 2*5
+puts '2'*5
+puts '2*5'
+puts 'You\'re swell!'
+puts 'Hello, world!'
+puts ' '
+puts 'Good-bye.'
